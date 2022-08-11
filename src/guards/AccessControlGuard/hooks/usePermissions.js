fbfa58288fa0cd2a6a63b4174cd94aa2a4643a53
Efrain Rodriguez <efrainrg92@gmail.com>
2022-08-11 16:45:18 -0500

    creating custom hook to retrieve user permissions

--- src/guards/AccessControlGuard/hooks/usePermissions.js
+++ src/guards/AccessControlGuard/hooks/usePermissions.js
@@ -1,3 +1,21 @@
-const usePermissions = () => [];
+import { useSelector } from 'react-redux';
+
+const usePermissions = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  const allPermissions = [];
+
+  if (user?.roles?.length) {
+    user.roles.forEach((role) => {
+      if (role.permissions?.length) {
+        role.permissions.forEach((permission) => {
+          allPermissions.push(permission);
+        });
+      }
+    });
+  }
+
+  return allPermissions;
+};
 
 export default usePermissions;
